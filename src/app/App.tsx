8433cfcba805f3e0c5e95365afa37e6748777e69
Nicole <nicole.grima.d56937@mcast.edu.mt>
2026-01-13 22:45:16 +0100

    Remove Vite demo content from App

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Professional Portfolio</h1>
+      <p>Advanced Front-End Development Assignment</p>
+    </div>
+  );
+}
+
+export default App;
