c7bdc7d82b11654131150c2d1d36f7731b0ee2db
Nicole <nicole.grima.d56937@mcast.edu.mt>
2026-01-13 23:03:58 +0100

    Add About page route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,19 @@
-import AppRoutes from '../routes/AppRoutes';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import HomePage from "../pages/HomePage";
+import AboutPage from "../pages/AboutPage";
+import ProjectsPage from "../pages/ProjectsPage";
+import ContactPage from "../pages/ContactPage";
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <h1>Professional Portfolio</h1>
-      <AppRoutes />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,13 @@
+const AboutPage = () => {
+  return (
+    <section>
+      <h1>About Me</h1>
+      <p>
+        This portfolio showcases my projects, skills, and experience in web
+        development.
+      </p>
+    </section>
+  );
+};
+
+export default AboutPage;
